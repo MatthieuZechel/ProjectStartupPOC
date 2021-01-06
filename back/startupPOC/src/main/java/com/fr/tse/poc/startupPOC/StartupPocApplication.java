ae2bbb0d2f592292a68f620e5eb4613381c7c43c
Matthieu Zechel <43524885+MatthieuZechel@users.noreply.github.com>
2021-01-06 15:08:00 +0100

    Add basis of backup generated by SpringInitializer

---
+++ back/startupPOC/src/main/java/com/fr/tse/poc/startupPOC/StartupPocApplication.java
@@ -0,0 +1,13 @@
+package com.fr.tse.poc.startupPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupPocApplication.class, args);
+	}
+
+}
---
+++ back/startupPOC/src/test/java/com/fr/tse/poc/startupPOC/StartupPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr.tse.poc.startupPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
